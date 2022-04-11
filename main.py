7670c6ce0ccf41f4d5d4c6b5b5a47c56b7c0da93
Sangco428 <98925358+Sangco428@users.noreply.github.com>
2022-04-11 11:31:32 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,34 +1,23 @@
 from tkinter import *
-from tkinter import ttk
-window = Tk()
+class MyWindow:
+    def __init__(self, win):
+        self.lbl1=Label(win, text='Enter your fullname')
+        self.txt1=Entry(bd=3)
+        self.txt2=Entry()
+        self.btn1 = Button(win, text="Click to display")
+        self.lbl1.place(x=70, y=50)
+        self.txt1.place(x=200, y=50)
+        self.txt2.place(x=200, y=100)
+        self.b1=Button(win, text='Click to display', command=self.name)
+        self.b1.place(x=70, y=100)
 
-window.geometry("500x300+10+20")
-window.title("Welcome to Python Programming")
+    def name(self):
+        fname=str(self.txt1.get())
+        result= fname
+        self.txt2.insert(END, str(result))
 
-button=Button(window,text = "Button", fg = "red", font = ("Helvetica", 16))
-button.place(x=50, y=70)
-
-label=Label(window,text = "This is a label", fg="blue", bg="orange")
-label.place(x=100, y=170)
-
-txtfld = Entry(window,text = "This is a text field", bd = 5)
-txtfld.place(x=210, y=170)
-
-v1=IntVar()
-radiobutton= Radiobutton(window, text = "Male",variable=v1, value = 1)
-radiobutton1= Radiobutton(window, text = "female",variable=v1, value = 0)
-radiobutton.place(x=30, y=40)
-radiobutton1.place(x=30)
-
-v2=StringVar()
-v2.set("Student1")
-data1= "Student1", "Student2", "Student3"
-combo= ttk.Combobox(window, values=data1)
-combo.place(x=360)
-
-data= "Student1", "Student2", "Student3"
-lb = Listbox(window, height=5,selectmode="multiple")
-for num in data:
-    lb.insert(END, num)
-lb.place(x=210)
+window=Tk()
+mywin=MyWindow(window)
+window.title('Midterm in OOP')
+window.geometry("400x300+10+10")
 window.mainloop()
